58e4dd8fae095d58d6914dc55680e09c5c29f0a8
kayne10 <troy.kayne@colorado.edu>
2021-09-28 21:46:49 -0600

    env vars are now accessible in docker container

--- setup_local_dev.sh
+++ setup_local_dev.sh
@@ -7,6 +7,7 @@ readonly DIR=$(pwd)
 source "${DIR}/local.env"
 
 TAG_NAME=""
+REGION=""
 
 while getopts ":t:" opt; do
   case $opt in
@@ -16,12 +17,12 @@ while getopts ":t:" opt; do
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
-      echo "Usage: ./setup_local_dev.sh -t <tag_name> [optional]"
+      echo "Usage: ./setup_local_dev.sh -t <tag_name> [optional] -r <region> [optional]"
       exit 1
       ;;
     :)
       echo "Option -$OPTARG requires an argument." >&2
-      echo "Usage: ./setup_local_dev.sh -t <tag_name> [optional]"
+      echo "Usage: ./setup_local_dev.sh -t <tag_name> [optional] -r <region> [optional]"
       exit 1
       ;;
   esac
@@ -31,12 +32,20 @@ done
 if [[ -z "$TAG_NAME" || "$TAG_NAME" == "" ]]
 then
     echo "No image specified. Pulling latest image from ECR"
+    aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin "${ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com"
     docker pull $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/airflow:latest
     export IMAGE="$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/airflow:latest"
 
 # Build docker image if tag name is specified
 else
     echo "Building: ${TAG_NAME}"
+
+    while IFS= read -r line; do
+        echo $line
+        opts+=(--build-arg "$line")
+    done < "local.env"
+
+    # docker build "${opts[@]}" -t airflow:$TAG_NAME .
     docker build -t airflow:$TAG_NAME .
     echo "Docker image built successfully. Containers are ready :)"
     export IMAGE="airflow:$TAG_NAME"
